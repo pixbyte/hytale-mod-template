3e29669561b0607cb39cd38aff5983e676debdfa
Dommi <dommihd@gmail.com>
2026-01-16 20:58:31 +0100

    initial commit

--- README.md
+++ README.md
@@ -1,2 +1,9 @@
 # hytale-mod-template
 Template for creating a new hytale mod.
+
+## Getting started
+
+- set the HYTALE_PATH environment variable with your path to the hytale install
+
+## Todos:
+- maven archetype to generate this template
---
+++ src/main/java/com/example/hytale/ExamplePlugin.java
@@ -0,0 +1,16 @@
+package com.example.hytale;
+
+import com.hypixel.hytale.server.core.plugin.JavaPlugin;
+import com.hypixel.hytale.server.core.plugin.JavaPluginInit;
+
+public class ExamplePlugin extends JavaPlugin {
+
+    public ExamplePlugin(JavaPluginInit init) {
+        super(init);
+    }
+
+    @Override
+    protected void setup() {
+
+    }
+}
